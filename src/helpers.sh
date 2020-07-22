f02b7e7b7aef0328fb25a724fa55f4f5c2073326
Byeong Kim <byeong.kim0430@gmail.com>
2020-07-22 17:19:47 -0600

    Feature/refactor (#2)
    * Add `.DS_Store`
    
    * Add more env variables
    
    * Create and use `helpers.sh`

---
+++ src/helpers.sh
@@ -0,0 +1,70 @@
+function export_env_var {
+  # $1: `.env` file directory
+
+  if [ -f $1 ] 
+  then
+    export $(cat $1 | sed 's/#.*//g' | xargs)
+  fi
+}
+
+function cleanup {
+  echo "Exiting."
+  exit 0
+}
+
+function take_photo {
+  # $1: Delay in seconds
+  # $2: Full image filename
+  # $3: Width resolution
+  # $4: Height resolution
+  # raspistill reference: https://www.raspberrypi.org/documentation/usage/camera/raspicam/raspistill.md
+
+  raspistill --nopreview -t $1 -o $2 -w $3 -h $4
+}
+
+function create_image_folder {
+  # $1: Image file index number
+
+  DATE=$(date +%Y-%m-%d_%H-%M-%S)
+
+  # Create folder for current timelapse set
+  if [ $1 -eq 0 ]; then
+    FOLDER_NAME=imgs/$DATE
+    mkdir -p $FOLDER_NAME
+  fi
+
+  FOLDER_FILE_NAME=$FOLDER_NAME/"${DATE}_(${1}).jpg"
+
+  echo $FOLDER_FILE_NAME
+}
+
+function add_image_to_s3 {
+  # $2: Image folder + file name
+  
+  aws s3 cp $(pwd)/$2 $1/$2
+}
+
+function analyze_image {  
+  # $1: Python file to run
+  # $2: Full image name
+
+  source $(pwd)/env/bin/activate
+
+  python $(pwd)/$1 $2
+
+  echo "Python closed."
+
+  deactivate
+}
+
+function turn_light_on {
+  ON_STATUS="$(curl -I ${1} | head -n 1 | cut -d$' ' -f2)"
+
+  echo $ON_STATUS
+}
+
+function turn_light_off {
+  OFF_STATUS="$(curl -I ${1} | head -n 1 | cut -d$' ' -f2)"
+
+  echo $OFF_STATUS
+}
--- src/timelapse.sh
+++ src/timelapse.sh
@@ -2,94 +2,66 @@
 # Calls 'raspistill' every 300s (ie. 5 mins) and writes the images
 # to a new date/timestamped folder.
 
-ENV_FILE_DIR=../.env
+source ./helpers.sh
 
-if [ -f $ENV_FILE_DIR ] 
-then
-  export $(cat $ENV_FILE_DIR | sed 's/#.*//g' | xargs)
-fi
+export_env_var ../.env
 
-# Light on and off variables
-LIGHT_ON=https://maker.ifttt.com/trigger/sourdough_on/with/key/${KEY}
-LIGHT_OFF=https://maker.ifttt.com/trigger/sourdough_off/with/key/${KEY}
+# Create image root folder if not exist
+mkdir -p imgs 
 
-# Delay variables
-TOTAL_DELAY=300 # in seconds
-CAM_DELAY=1     # need to have a nonzero delay for raspistill
+# Set initial image index
+IDX=0 
 
-# Must be 1.33 ratio
-RES_W=1440
-RES_H=1080
+# Calculate width resolution (RES_W : RES_H = 1.33)
+RES_W=$(($RES_H * 133 / 100))
 
 # Calculate the total delay time per cycle
 SLEEP_DELAY=$(($TOTAL_DELAY - $CAM_DELAY))
 
-FOLDER_NAME=imgs
-mkdir -p $FOLDER_NAME # create image root folder if not exist
-
-IDX=0 # image index
-
-function cleanup() {
-  echo "Exiting."
-  exit 0
-}
-
 trap cleanup INT
 
-while true; do
-  DATE=$(date +%Y-%m-%d_%H-%M-%S)
-  FNAME="${DATE}_(${IDX})" # image filename
+function main {
+  # Delay so the light is completely turned on when a photo is taken
+  sleep 2
 
-  # Create folder for current timelapse set
-  if [ $IDX -eq 0 ]; then
-    FOLDER_NAME=$FOLDER_NAME/$DATE
-    mkdir -p $FOLDER_NAME
-    echo "Created folder: ${FOLDER_NAME}"
+  # Take image
+  take_photo $CAM_DELAY $(pwd)/$1 $RES_W $RES_H
+
+  echo "Captured: ${FILE_NAME}"
+
+  IDX=$((IDX + 1))
+
+  sleep 2
+
+  if [ -n "$S3_BUCKET_ENDPOINT" ]
+  then
+    add_image_to_s3 $S3_BUCKET_ENDPOINT $1
   fi
 
-  # Turn light on
-  ON_STATUS="$(curl -I ${LIGHT_ON} | head -n 1 | cut -d$' ' -f2)"
+  analyze_image "examine_single_file.py" $(pwd)/$1
 
-  if [ $ON_STATUS -eq 200 ]
+  sleep $SLEEP_DELAY
+}
+
+while true; do
+  FOLDER_FILE_NAME=$(create_image_folder ${IDX})
+
+  if [ -n "$LIGHT_ON_ENDPOINT" ] && [ -n "$LIGHT_OFF_ENDPOINT" ] 
   then
-    echo "Light turned on"
+    # Turn light on
+    ON_STATUS=$(turn_light_on ${LIGHT_ON_ENDPOINT})
 
-    # Delay so the light is completely turned on when a photo is taken
-    sleep 2
-
-    # Take image
-    raspistill --nopreview -t $CAM_DELAY -o ./$FOLDER_NAME/$FNAME.jpg -w $RES_W -h $RES_H
-
-    echo "Captured: ${FNAME}"
-    IDX=$((IDX + 1))
-
-    sleep 2
-
-    # Turn light off
-    OFF_STATUS="$(curl -I ${LIGHT_OFF} | head -n 1 | cut -d$' ' -f2)"
-
-    if [ $OFF_STATUS -eq 200 ] 
+    if [ $ON_STATUS -eq 200 ]
     then
-      echo "Light turned off"
+      echo "Light turned on"
+      main $FOLDER_FILE_NAME
+
+      # Turn light off
+      OFF_STATUS=$(turn_light_off ${LIGHT_OFF_ENDPOINT})
     else
-      echo "Light did NOT turn off"
+      echo "Light did not turn on"
     fi
-
-    # Copy the image to S3
-    ORIGINAL_DIR=$(pwd)/$FOLDER_NAME/$FNAME.jpg
-    
-    aws s3 cp $ORIGINAL_DIR $S3_BUCKET/$FOLDER_NAME/$FNAME.jpg
-
-    source env/bin/activate
-
-    python env/examine_single_file.py $PWD/$FOLDER_NAME/$FNAME.jpg
-
-    echo "Python closed."
-
-    deactivate
-
-    sleep $SLEEP_DELAY
   else
-    echo "Light did NOT turn on"
+    main $FOLDER_FILE_NAME
   fi
 done
